8641aacc13e7f11e74f6ad6490d88c17f29c19a8
GameDevUv <gamedevelopmentuv@gmail.com>
2025-02-26 15:57:08 +0530

    Some ui Changes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -17,7 +17,7 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  // methods: ['GET', 'POST', 'PUT', 'DELETE'],
   // allowedHeaders: ['Content-Type', 'Authorization']
 }));
 // app.use(cors());
