032a3a238f8d89fccdd286f804185df3a9938f90
GameDevUv <gamedevelopmentuv@gmail.com>
2025-03-01 22:44:16 +0530

    high

--- App/src/index.js
+++ App/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./app/store";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store} >
-    <BrowserRouter >
+    <Router >
       <App />
-    </BrowserRouter>
+    </Router>
   </Provider>
 
 );
